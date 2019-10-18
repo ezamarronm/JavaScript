00a9474a8e9275852c3326d7c5c993bfde0ff3f4
ZamarronUbuntuLenovo <eduardo.zamarron.m@gmail.com>
2019-10-17 22:29:56 -0500

    JS Functions as Parameters

--- 02JavaScriptFundamentals/18Classes.js
+++ 02JavaScriptFundamentals/18Classes.js
@@ -1,25 +1,41 @@
+function extendsOf(sonPrototype, fatherPrototype){
+   var fn = function () {}
+   fn.prototype = fatherPrototype.prototype
+   sonPrototype.prototype = new fn
+   sonPrototype.prototype.constructor = sonPrototype
+}
+
 function Person(name,lastName, height) {
    this.name = name
-   this.lastname = lastName
+   this.lastName = lastName
    this.height = height
 }
 Person.prototype.greet = function(){
-   console.log(`Hi I'm ${this.name} ${this.lastname}`)
+   console.log(`Hi I'm ${this.name} ${this.lastName}`)
 }
 
 Person.prototype.isTall = function() {
-   if(this.height > 1.7)
-      console.log(`I'm Tall`)
-   else
-      console.log(`I'm not Tall`)
-
+   return this.height > 1.7
 }
-var eduardo = new Person('Eduardo','Zamarron',1.68)
+
+function Developer(name, lastName){
+   this.name = name
+   this.lastName = lastName
+}
+extendsOf(Developer, Person)
+
+
+Developer.prototype.greet = function () {
+   console.log(`Hello I'm ${this.name} ${this.lastName} and I'm a Developer`)
+}
+
+
+/* var eduardo = new Person('Eduardo','Zamarron',1.68)
 var brenda = new Person('Brenda','Hernandez',1.58)
-var charly = new Person('Carlos','Blanco',1.71)
-eduardo.greet()
-eduardo.isTall()
+var charly = new Person('Carlos','Blanco',1.71) */
+/* eduardo.greet()
+console.log(eduardo.isTall())
 brenda.greet()
-brenda.isTall()
+console.log(brenda.isTall())
 charly.greet()
-charly.isTall()
+console.log(charly.isTall()) */
