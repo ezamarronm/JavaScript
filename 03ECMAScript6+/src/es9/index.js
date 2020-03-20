8b9f938a85a4d42d56d92864e776384f21edfd8e
ZamarronUbuntuLenovo <eduardo.zamarron.m@gmail.com>
2020-03-19 21:21:05 -0600

    Course finished

--- 03ECMAScript6+/src/es9/index.js
+++ 03ECMAScript6+/src/es9/index.js
@@ -44,7 +44,6 @@
 /**
  * Regex
  */
-const regexData = /([0-9]{4})-([0-9]{2})-([0-9]{2})/
 const regexData = /([0-9]{4})-([0-9]{2})-([0-9]{2})/; 
 const match = regexData.exec('2018-04-20');
 const year = match[1];
